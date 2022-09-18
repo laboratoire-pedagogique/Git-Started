a9edbc89fd919c565add21c2d3fb8392cfa399df
Sanmandresy <hei.sanda.3@gmail.com>
2022-09-18 18:05:41 +0300

    feat: handling name prompt

--- js/index.js
+++ js/index.js
@@ -1,14 +1,44 @@
+// Necessary elements
 const input = document.getElementById("name-prompt");
 const container = document.getElementById("container");
+const spinner = document.getElementById("spinner");
+const greeting = document.getElementById("greeting");
 
 
-const alertName = (input) => {
-    alert(`Hello ${input.value} !`);
-    input.value = "";
+const resetValue = (element) => {
+    element.value = "";
+}
+
+const startLoading = (input,spinner) => {
+    input.classList.add("hidden");
+    spinner.classList.add("spinner");
+}
+
+const stopLoading =  (spinner) => {
+    setTimeout(() => {
+        spinner.classList.remove("spinner");
+        greeting.classList.remove("hidden");
+        greeting.classList.add("greeting");
+    },2000);
+}
+
+const greet = (input) => {
+    // Change the p tags text
+    greeting.innerText = `Hello ${input.value} !`;
+    
+    // Change the input text to blank 
+    resetValue(input);
+
+    // Loader is on
+    startLoading(input,spinner)
+
+    // Waiting 2s before removing the loader
+    stopLoading(spinner);
 }
 
 input.addEventListener("keydown",(event) => {
+    // When "Enter" is pressed
     if(event.key == "Enter"){
-        alertName(input);
+        greet(input);
     }
 })
