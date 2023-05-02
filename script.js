7c3fe63748fc58f4db2bd21506eb4a8202ccddb4
Diary <hei.diary.2@gmail.com>
2023-05-02 08:59:02 +0300

    My TD5 exo5 Web1

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const incrementButton = document.getElementById('incrementButton');
+const counter = document.getElementById('counter');
+
+let count = 0;
+
+incrementButton.addEventListener('click', () => {
+  count++;
+  counter.innerText = count;
+});
+
